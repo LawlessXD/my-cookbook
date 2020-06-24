376cdce26ebf28ce4f666abb42c23bc75c29f218
LawlessXD <DavidLawless@gmail.com>
2020-06-24 13:57:49 +0100

    Added login, logout, and register functionality

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, flash, render_template, redirect, request, url_for, session
 from flask_pymongo import PyMongo
+from werkzeug.security import generate_password_hash, check_password_hash
 from bson.objectid import ObjectId
 from os import path
 if path.exists("env.py"):
@@ -8,13 +9,14 @@ if path.exists("env.py"):
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = "myCookBook"
-MONGODB_URI = os.environ.get("MONGO_URI") 
-app.config["MONGO_URI"] = MONGODB_URI
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME") 
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI") 
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
 @app.route('/')
+@app.route('/index')
 @app.route('/recipes')
 def get_recipes():
     return render_template("recipes.html", recipes=mongo.db.recipes.find())
@@ -22,6 +24,94 @@ def get_recipes():
 @app.route('/recipe/<recipe_id>')
 def view_recipe(recipe_id):
     return render_template("viewrecipe.html")
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    # https://flask.palletsprojects.com/en/1.1.x/quickstart/
+    if 'user' in session:
+        isUser = mongo.db.users.find_one({'username': session['user']})
+        if isUser:
+            flash(f"You are already logged in as {session['user']}, please logout to sign in as a different user.")
+            return redirect(url_for('get_recipes'))
+    if request.method == 'POST':
+        # retrieve the data from the login form
+        username = request.form.get('username').lower()
+        password = request.form.get('password').lower()
+        # Check if user is in the DB
+        isUser = mongo.db.users.find_one({'username': username})
+        if isUser:
+            # Check if the password entered matches the passowrd in DB
+            if check_password_hash(isUser['password'],password):
+                # Add username to session variable user
+                session['user'] = username
+                session['first'] = isUser['firstname']
+                flash("You have been successfully logged in!")
+                return redirect(url_for('get_recipes'))
+            else:
+                flash("Invalid Password, please check and try again!")
+                return redirect(url_for('login'))
+        else:
+            flash("Invalid Username, please check and try again!")    
+            return redirect(url_for('login'))
+        # show the login form    
+    return render_template("login.html")
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    # Check if user is not logged in already
+    if 'user' in session:
+        flash('You are already signed in')
+        return redirect(url_for('get_recipes'))
+    # https://flask.palletsprojects.com/en/1.1.x/quickstart/
+    if request.method == 'POST':
+        # retrieve the data from the registration form
+        username = request.form.get('username').lower()
+        password = request.form.get('password').lower()    
+        confirm_password = request.form.get('confirm_password').lower()
+        firstname = request.form.get('firstname').lower()
+        lastname = request.form.get('lastname').lower()
+        email = request.form.get('email').lower()
+        # Check if the passwords entered match
+        if password== confirm_password:
+            # Check the username does not exist already
+            isUser = mongo.db.users.find_one({'username': username})
+            if isUser:
+                flash(f"{username} already exists, please choose a different username.")
+                return redirect(url_for('register'))
+            # If user does not exist then add the user to the DB
+            else:
+                hashed_password = generate_password_hash(password,method='pbkdf2:sha256')
+                mongo.db.users.insert_one(
+                    {
+                        'username': username,
+                        'password': hashed_password,
+                        'firstname': firstname,
+                        'lastname': lastname,
+                        'email': email
+                    }
+                )
+                # Check if the user has been saved in the DB
+                isUser = mongo.db.users.find_one({'username': username})
+                if isUser:
+                    # Add user to the session variable User
+                    session['user'] = username
+                    return redirect(url_for('get_recipes'))
+                else:
+                    flash("There was an issue saving your details, please try again.")
+                    return redirect(url_for('register'))
+        else:
+            flash("Passwords entered do not match, please verify and try again.")
+            return redirect(url_for('register'))
+
+    return render_template("register.html") 
+
+# Log out
+@app.route('/logout')
+def logout():
+	# Clear the session
+	session.clear()
+	flash('You have been successfully logged out, see you again soon!')
+	return redirect(url_for('get_recipes'))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
